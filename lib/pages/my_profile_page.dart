551a3001fcd732d7e27d81819a409c5615066824
abduqahhor-001 <abduqahhora364@gmail.com>
2024-08-21 12:21:16 +0500

    Added new_file.txt

--- lib/pages/my_profile_page.dart
+++ lib/pages/my_profile_page.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../model/post_model.dart';
+
 class MyProfilePage extends StatefulWidget {
   const MyProfilePage({super.key});
 
@@ -7,12 +14,329 @@ class MyProfilePage extends StatefulWidget {
 }
 
 class _MyProfilePageState extends State<MyProfilePage> {
+  bool isLoading = false;
+  int axisCount = 2;
+  List<Post> items = [];
+  final ImagePicker _picker = ImagePicker();
+  File? _image;
+  String fullname = "Abduqahhor",
+      email = "abduqahhora364@gmail.com",
+      img_url = "";
+  int count_posts = 0, count_followers = 0, count_following = 0;
+  String post_img1 =
+      "https://images.unsplash.com/photo-1721332155637-8b339526cf4c?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  String post_img2 =
+      "https://images.unsplash.com/photo-1723754166148-2ceb2d84890f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  String post_img3 =
+      "https://images.unsplash.com/photo-1723754166148-2ceb2d84890f?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  _imgFromGallery() async {
+    XFile? image =
+    await _picker.pickImage(source: ImageSource.gallery, imageQuality: 50);
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
+
+  _imgFromCamera() async {
+    XFile? image =
+    await _picker.pickImage(source: ImageSource.camera, imageQuality: 50);
+    setState(() {
+      _image = File(image!.path);
+    });
+  }
+
+  void _showPicker(context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return SafeArea(
+            child: Container(
+              child: Wrap(
+                children: [
+                  ListTile(
+                      leading: new Icon(Icons.photo_library),
+                      title: new Text('Pick Photo'),
+                      onTap: () {
+                        _imgFromGallery();
+                        Navigator.of(context).pop();
+                      }),
+                  ListTile(
+                    leading: new Icon(Icons.photo_camera),
+                    title: new Text('Take Photo'),
+                    onTap: () {
+                      _imgFromCamera();
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+  @override
+  void initState() {
+    items.add(Post(post_img1, "Discover more great images on our sponsor's site"));
+    items.add(Post(post_img2, "Discover more great images on our sponsor's site"));
+    items.add(Post(post_img3, "Discover more great images on our sponsor's site"));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("profile page"),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          "Profile",
+          style: TextStyle(
+              color: Colors.black, fontFamily: "Billabong", fontSize: 25),
+        ),
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.exit_to_app),
+            color: Color.fromRGBO(193, 53, 132, 1),
+          )
+        ],
+      ),
+      body: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(10),
+            child: Column(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    _showPicker(context);
+                  },
+                  child: Stack(
+                    children: [
+                      Container(
+                        padding: EdgeInsets.all(2),
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(70),
+                            border: Border.all(
+                              width: 1.5,
+                              color: Color.fromRGBO(193, 53, 132, 1),
+                            )),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(35),
+                          child:_image == null ? Image(
+                            image: AssetImage("assets/images/ic_person.png"),
+                            width: 70,
+                            height: 70,
+                            fit: BoxFit.cover,
+                          ):Image.file(
+                            _image!,
+                            width:70 ,
+                            height: 70,
+                            fit: BoxFit.cover,
+                          )
+                        ),
+                      ),
+                      Container(
+                        width: 80,
+                        height: 80,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          children: [
+                            Icon(
+                              Icons.add_circle,
+                              color: Colors.purple,
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  fullname.toUpperCase(),
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold),
+                ),
+                SizedBox(
+                  height: 3,
+                ),
+                Text(
+                  email.toUpperCase(),
+                  style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 14,
+                      fontWeight: FontWeight.normal),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 10),
+                  height: 80,
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Text(
+                                  count_posts.toString(),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 3,
+                                ),
+                                Text(
+                                  "Posts",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                      Expanded(
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Text(
+                                  count_followers.toString(),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 3,
+                                ),
+                                Text(
+                                  "Followers",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                      Expanded(
+                          child: Center(
+                            child: Column(
+                              children: [
+                                Text(
+                                  count_following.toString(),
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                SizedBox(
+                                  height: 3,
+                                ),
+                                Text(
+                                  "Following",
+                                  style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.normal,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )),
+                    ],
+                  ),
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Center(
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              axisCount = 1;
+                            });
+                          },
+                          icon: Icon(Icons.list_alt),
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: Center(
+                        child: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              axisCount = 2;
+                            });
+                          },
+                          icon: Icon(Icons.grid_view),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Expanded(
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: axisCount),
+                    itemCount: items.length,
+                    itemBuilder: (ctx, index) {
+                      return _itemOfPost(items[index]);
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
+}
+
+Widget _itemOfPost(Post post) {
+  return GestureDetector(
+    onLongPress: () {},
+    child: Container(
+      margin: EdgeInsets.all(5),
+      child: Column(
+        children: [
+          Expanded(
+            child: CachedNetworkImage(
+              width: double.infinity,
+              imageUrl: post.img_post,
+              placeholder: (context, url) => Center(
+                child: CircularProgressIndicator(),
+              ),
+              errorWidget: (context, url, error) => Icon(Icons.error),
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            height: 3,
+          ),
+          Text(
+            post.caption,
+            style: TextStyle(color: Colors.black87.withOpacity(0.7)),
+            maxLines: 2,
+          )
+        ],
+      ),
+    ),
+  );
 }
--- lib/pages/my_search_page.dart
+++ lib/pages/my_search_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_myinsta/model/member_model.dart';
 class MySearchPage extends StatefulWidget {
   const MySearchPage({super.key});
 
@@ -7,12 +8,135 @@ class MySearchPage extends StatefulWidget {
 }
 
 class _MySearchPageState extends State<MySearchPage> {
+  bool isLoading = false;
+  var searchController = TextEditingController();
+  List<Member> item = [];
+
+  @override
+  void initState() {
+    
+    super.initState();
+    item.add(Member("Abduqahhor", "abduqahhora364@gmail.com"));
+    item.add(Member("Begzod", "begzod@gmail.com"));
+    item.add(Member("Oybek", "oybek@gmail.com"));
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("search page"),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text("Serach" , style: TextStyle(color: Colors.black , fontFamily: "Billabong",fontSize: 25),),
+
       ),
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 20,right: 20),
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(bottom: 10),
+                  padding: EdgeInsets.only(left: 10,right: 10),
+                  height: 45,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.withOpacity(0.2),
+                    borderRadius: BorderRadius.circular(7),
+                  ),
+                  child: TextField(
+                      style: TextStyle(color: Colors.black87),
+                    controller: SearchController(),
+                    decoration: InputDecoration(
+                      hintText: "Search",
+                      border: InputBorder.none,
+                      hintStyle: TextStyle(fontSize: 15,color: Colors.grey),
+                      icon: Icon(Icons.search , color: Colors.grey,),
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                    child:ListView.builder(
+                      itemCount: item.length,
+                        itemBuilder: (ctx , index){
+                        return _itemOfMember(item[index]);
+                        }
+                    ),
+                ),
+
+              ],
+            ),
+          )
+        ],
+      )
     );
   }
+}
+
+Widget _itemOfMember(Member member){
+  return Container(
+    height: 90,
+    child: Row(
+      children: [
+        Container(
+          padding: EdgeInsets.all(2),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(70),
+            border: Border.all(
+              width: 1.5,
+              color: Color.fromRGBO(193,53,132,1),
+
+            )
+          ),
+          child: ClipRRect(
+            borderRadius:BorderRadius.circular(22.5) ,
+            child:Image(
+                image: AssetImage("assets/images/ic_person.png"),
+              width: 45,
+              height: 45,
+              fit: BoxFit.cover,
+            ) ,
+          ),
+        ),
+        SizedBox(width: 15,),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(member.fullname,style: TextStyle(fontWeight: FontWeight.bold,),),
+            SizedBox(height: 3,),
+            Text(member.email,style: TextStyle(color: Colors.black54),),
+
+          ],
+        ),
+
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+             GestureDetector(
+               onTap:(){},
+               child:  Container(
+                 width: 100,
+                 height: 30,
+                 decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(3),
+                     border: Border.all(
+                         width: 1,
+                         color: Colors.grey
+                     )
+                 ),
+                 child: Center(
+                   child: Text("Follow"),
+                 ),
+               ),
+             )
+            ],
+          ),
+        ),
+      ],
+    ),
+  );
 }
